79e66dd0aba902a4e1f4585b33321f8b42771ce3
Valera <valerapro.66@yandex.ru>
2023-07-30 12:34:16 +0300

    sprint 9

---
+++ src/test/java/ru/yandex/sprint9/FilmorateApplicationTests.java
@@ -0,0 +1,10 @@
+package ru.yandex.sprint9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmorateApplicationTests {
+
+
+}

ab75e44f2b2af6bbe11bc70cf99ab97107b78746
Valera <valerapro.66@yandex.ru>
2023-08-16 22:35:49 +0300

    sprint 10

---
+++ src/test/java/ru/yandex/practicum/controler/FilmControllerTest.java
@@ -0,0 +1,41 @@
+package ru.yandex.practicum.controler;
+
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.model.Film;
+import ru.yandex.practicum.service.FilmService;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilmControllerTest {
+    FilmService filmService;
+    private final FilmController filmController = new FilmController(filmService);
+    Film film = Film.builder()
+            .id(1L)
+            .name("nameFilm")
+            .duration(100)
+            .releaseDate(LocalDate.of(2000, 10, 10))
+            .description("description")
+            .build();
+
+    @Test
+    void addFilm() {
+        filmController.addFilm(film);
+        assertNotNull(filmController.getAllFilms());
+    }
+
+    @Test
+    void updateUser() {
+        Film film1 = Film.builder()
+                .id(1L)
+                .name("name")
+                .duration(100)
+                .releaseDate(LocalDate.of(2000, 10, 10))
+                .description("description")
+                .build();
+        filmController.addFilm(film);
+        filmController.updateUser(film1);
+        assertEquals(filmController.getAllFilms().size(), 1);
+    }
+}
